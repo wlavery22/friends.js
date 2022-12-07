5e170d3c9a9589477aa89fe59ae9fe1588b8f5fe
William Lavery <lavery_william@yahoo.com>
2022-12-07 12:06:28 -0500

    Add greeting

--- friends2.js
+++ friends2.js
@@ -1,6 +1,10 @@
-var friend = "brooke"
+var friend = "jared"
+var friend2 = "tim"
 if (friend === "brooke") {
     console.log("hello best friend")
-}   else {
+}   else if (friend2 === "tim") {
+    console.log("hello best friend tim")
+}
+    else {
     console.log("hello other friend")
 }
